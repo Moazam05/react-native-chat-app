0d7495fbc4cead127017ab8a09df2c837de3e3e1
Moazam05 <salmanmoazam08@gmail.com>
2025-01-24 17:30:30 +0500

    un necessary code removed

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
-import notifee from '@notifee/react-native';
 
 import AppNavigator from './src/routes/AppNavigator';
 import {persistor, store} from './src/redux/store';
 import ToastComponent from './src/components/ToastComponent';
 
 const App = () => {
-  useEffect(() => {
-    const requestPermission = async () => {
-      await notifee.requestPermission();
-    };
-    requestPermission();
-  }, []);
-
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
